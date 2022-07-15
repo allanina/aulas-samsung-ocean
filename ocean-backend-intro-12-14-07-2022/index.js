fea21dbcbceaa1d845dfe02147d8f9867c4db2c2
Allana Evellyn <allanaevellynm@gmail.com>
2022-07-15 20:25:44 -0300

    playing with http methods

--- ocean-backend-intro-12-14-07-2022/index.js
+++ ocean-backend-intro-12-14-07-2022/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+//registrar que estamos usando json no body da req
+app.use(express.json());
+
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
@@ -11,8 +14,30 @@ app.get('/oi', function (req, res) {
 
 const bts = ["Kim Namjoon", "Kim Seokjin", "Jung Hoseok", "Park Jimin", "Kim Taehyung", "Jeon Jungkook"]
 
+
+//ler tudo
 app.get("/bts", function (req, res) {
     res.send(bts)
+})
+
+//com id
+
+app.get("/bts/:id", function (req, res) {
+    res.send("Ler pelo ID");
+})
+
+//criar 
+app.post('/bts', function (req, res){
+    console.log(req.body)
+
+    //acessamos o valor enviado na req
+    const item = req.body.nome;
+
+    //insere o valor na lista
+    bts.push(item)
+
+    //exibe mensagem de sucesso
+    res.send("membro criado com sucesso")
 })
 
 app.listen(3000, function () {
